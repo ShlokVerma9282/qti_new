8dac1909b1e95c0e5ada9afc3fee2b0fa888a5c0
Shubhangi Agarwal <shubhi.agarwal019@gmail.com>
2024-03-31 10:06:11 +0530

    Accordion button icons added

--- src/Components/C_Component/ButtonSection.js
+++ src/Components/C_Component/ButtonSection.js
@@ -5,11 +5,13 @@ export default function Footer() {
                 <button className="p-4 rounded-md 
                 font-sans font-medium text-lg bg-indigo-100 text-indigo-500">
                     Preview
+                    <i className="fa fa-eye pl-2" style={{color: "rgb(99 102 241"}}></i>
                 </button>
                 <button className="col-span-2 p-4  
                 bg-gradient-to-tr from-blue-500 to bg-purple-500 
                 text-white rounded-md font-sans font-medium text-lg">
-                    Create Course
+                    Create Course 
+                    <i className="fa-solid fa-arrow-right pl-2" style={{color: "#fcfcfc"}}></i>
                 </button>
             </div>
         </div>
